25f44cb0ddf498c460d8c82168eb2f7e48e4c0f8
GuliyevaLala <lalaig@code.edu.az>
2021-12-05 04:17:49 +0530

    two days of no sleep

---
+++ Js/Motion.js
@@ -0,0 +1,11 @@
+// function myFunction(x) {
+//     if (x.matches) {
+//         $(".main").mouseenter(function () {
+//             $(this).children().last().css({ "display": "block" });
+//         })
+//         $(".main").mouseleave(function () {
+//             $(this).children().last().css({ "display": "none" });
+
+//         })
+
+//     }
